6c63a2942456050e4a69a3ea238385e1c13d7df0
Kenytt Avery <kavery@fullerton.edu>
2019-09-18 17:36:00 -0700

    Flask + SQLite3 Example

---
+++ flask-sqlite3/api.py
@@ -0,0 +1,102 @@
+# Science Fiction Novel API from "Creating Web APIs with Python and Flask"
+# <https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask>.
+# 
+# What's new:
+#
+#  * Database specified in app config file
+#
+#  * Includes features from "Using SQLite 3 with Flask"
+#    <https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/>
+#
+
+import flask
+from flask import request, jsonify, g
+import sqlite3
+
+
+app = flask.Flask(__name__)
+app.config.from_envvar('APP_CONFIG')
+
+
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(app.config['DATABASE'])
+        db.row_factory = make_dicts
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.cli.command('init')
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('books.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
+
+
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    all_books = query_db('SELECT * FROM books;')
+
+    return jsonify(all_books)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_filter():
+    query_parameters = request.args
+
+    id = query_parameters.get('id')
+    published = query_parameters.get('published')
+    author = query_parameters.get('author')
+
+    query = "SELECT * FROM books WHERE"
+    to_filter = []
+
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    if published:
+        query += ' published=? AND'
+        to_filter.append(published)
+    if author:
+        query += ' author=? AND'
+        to_filter.append(author)
+    if not (id or published or author):
+        return page_not_found(404)
+
+    query = query[:-4] + ';'
+
+    results = query_db(query, to_filter)
+
+    return jsonify(results)
