16aaeeb314da4a672bc67b0f88c3239792b69f3b
Kenytt Avery <kavery@fullerton.edu>
2021-09-29 18:11:56 -0700

    Add hello Hug example

---
+++ hug/hello/api.py
@@ -0,0 +1,20 @@
+# $ http POST localhost:8000/greet first_name=Prof last_name=Avery
+import hug
+
+
+@hug.static("/static")
+def server_static():
+    return ("static",)
+
+
+@hug.get("/", output=hug.output_format.html)
+def show_form():
+    hug.redirect.to("/static/form.html")
+
+
+@hug.post("/greet")
+def return_greeting(first_name, last_name):
+    full_name = f"{first_name} {last_name}"
+    print(full_name)
+
+    return {"name": full_name}
---
+++ hug/hello/app.py
@@ -0,0 +1,66 @@
+import hug
+
+
+@hug.get("/", output=hug.output_format.html)
+def show_form():
+    return """
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <title>Dynamically-generated form</title>
+            </head>
+            <body>
+                <form method="POST" action="/greet">
+                    <p>
+                        <label for="first">First name:</label>
+                        <input type="text" id="first" name="first_name" />
+                    </p>
+
+                    <p>
+                        <label for="last">Last name:</label>
+                        <input type="text" id="last" name="last_name" />
+                    </p>
+
+                    <p>
+                        <input type="submit" />
+                    </p>
+                </form>
+            </body>
+        </html>
+    """
+
+
+@hug.post("/greet", output=hug.output_format.html, parse_body=True)
+def show_greeting(first_name, last_name):
+    full_name = f"{first_name} {last_name}"
+    print(full_name)
+
+    return f"""
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <title>Dynamically-generated form</title>
+            </head>
+            <body>
+                <form method="POST" action="/greet">
+                    <p>
+                        <label for="first">First name:</label>
+                        <input type="text" id="first" name="first_name" />
+                    </p>
+
+                    <p>
+                        <label for="last">Last name:</label>
+                        <input type="text" id="last" name="last_name" />
+                    </p>
+
+                    <p>
+                        <input type="submit" />
+                    </p>
+                </form>
+
+                    <p>
+                        Hello, {full_name}!
+                    </p>
+            </body>
+        </html>
+    """
